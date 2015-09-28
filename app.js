e69cfd3eefa07cf6b3b70da73dfc42dc0a2722ef
Forrest Spencer Buster <fspencerb@gmail.com>
2015-09-28 16:14:19 -0600

    some frogs and stuff

--- app.js
+++ app.js
@@ -5,10 +5,37 @@ app.controller('MainController', MainController);
 function MainController() {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
 
-}
+    function Guy(name, startingCash){
+        this.name = name;
+        this.cash = startingCash;
+        this.giveCash = function(amount) {
+            if (amount <= this.cash && amount > 0) {
+                this.cash = this.cash - amount;
+                return amount;
+            } else {
+                alert("I don't have enough cash to give you " + amount + ". " + this.name + " says...");
+                return 0;
+            }
+        };
+        this.receiveCash = function(amount) {
+            if (amount > 0) {
+                this.cash = this.cash + amount;
+                return amount;
+            } else {
+                alert(amount + " isn't an amount I'll take " + this.name + " says...");
+                return 0;
+            }
+        }
+    }
+    vm.giveMoneyToJoe = function() {
+        if (vm.bank >= 10) {
+            vm.bank -= vm.joe.receiveCash(10)
+        } else {
+            alert("The bank is out of money.")
+        }
+    }
 
-var CreateaGuyClass = function (){
-    function giveCash() {
-        
+    vm.receiveMoneyFromBob = function() {
+        vm.bank += vm.bob.giveCash(5)
     }
 }
